d27ee379a871e31d0e85e3bd27262180a1634a0a
zai-i <zainab@wonderbly.com>
2021-10-03 21:23:35 +0100

    fix

--- src/components/reload-prompt.jsx
+++ src/components/reload-prompt.jsx
@@ -1,8 +1,14 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react'
+
 import { useRegisterSW } from 'virtual:pwa-register/react'
 
-export default function ReloadPrompt() {
+function ReloadPrompt() {
 
   const {
+    offlineReady: [offlineReady, setOfflineReady],
+    needRefresh: [needRefresh, setNeedRefresh],
+    updateServiceWorker,
   } = useRegisterSW({
     onRegistered(r) {
       // eslint-disable-next-line prefer-template
@@ -13,4 +19,27 @@ export default function ReloadPrompt() {
     },
   })
 
+  const close = () => {
+    setOfflineReady(false)
+    setNeedRefresh(false)
+  }
+
+  return (
+    <div className="ReloadPrompt-container">
+      { (offlineReady || needRefresh)
+          && <div className="ReloadPrompt-toast">
+            <div className="ReloadPrompt-message">
+              { offlineReady
+                ? <span>App ready to work offline</span>
+                : <span>New content available, click on reload button to update.</span>
+              }
+            </div>
+            { needRefresh && <button className="ReloadPrompt-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button> }
+            <button className="ReloadPrompt-toast-button" onClick={() => close()}>Close</button>
+          </div>
+      }
+    </div>
+  )
 }
+
+export default ReloadPrompt
