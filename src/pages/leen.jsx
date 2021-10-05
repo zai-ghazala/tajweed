3fe6b742f4bcd47772c624aee57694f8b389ee23
zai-i <zainab@wonderbly.com>
2021-10-05 18:51:00 +0100

    typo

--- src/pages/leen.jsx
+++ src/pages/leen.jsx
@@ -12,7 +12,7 @@ export default function Leen() {
     <ScrollToTop/>
     <div className="tajweed-group">
             <h1><span className="arabic">اللین</span> Al Leen</h1><p><b>Pronouncing the letter with ease, without jerking or raising
-            the voice</b></p><p><span className="arabic">ي</span>          
+            the voice</b></p><p><span className="arabic">ي</span>{" "}          
             and <span className="arabic">و</span> sakinah preceded with
             fatha</p>
             </div>
