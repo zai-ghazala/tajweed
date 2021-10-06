833c5016697a63e8f3f09d4b6bcd2cedf9fed89c
zai-i <zainab@wonderbly.com>
2021-10-06 20:19:25 +0100

    rewording

--- src/components/nav-content.jsx
+++ src/components/nav-content.jsx
@@ -7,7 +7,7 @@ export default function NavContent() {
             <p><b>Add to home screen ✨</b></p>
 <p><b>On iOS</b> click the share button then ‘add to home screen’</p>
 <p><b>On Android</b> click the ⋮ menu button then ‘install app’</p>
-<p>This app can work offline</p>
+<p>This app also works offline</p>
             
             <p><b>Sources</b></p>
             <p>Illustrations by Eman Atef</p>
