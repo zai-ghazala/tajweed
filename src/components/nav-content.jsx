a1a51044311e38d2c20f07e625680183d43d1411
zai-i <zainab@wonderbly.com>
2021-10-08 14:57:58 +0100

    reorder text nav-content

--- src/components/nav-content.jsx
+++ src/components/nav-content.jsx
@@ -4,13 +4,12 @@ import { Link } from "wouter";
 export default function NavContent() {
   return (<div className="nav-content">
           <div className="sources">
+            
+<p>Feel free to contact me at <a className="email" href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a> with any corrections or suggestions</p>
             <p><b>Add to home screen ✨</b></p>
 <p><b>On iOS</b> click the share button then ‘add to home screen’</p>
 <p><b>On Android</b> click the ⋮ menu button then ‘install app’</p>
 <p>This app also works offline</p>
-
-<p>Feel free to contact me at <a className="email" href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a> with any corrections or suggestions</p>
-            
             <p><b>Sources</b></p>
             <p>Illustrations by Eman Atef</p>
             <p>Notes based on Sheikh Ayman Suwayd’s material</p>
