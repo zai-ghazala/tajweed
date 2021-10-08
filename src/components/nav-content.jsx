afaf4f6b7e9db33260b6afda2d2d30f8311d6a40
zai-i <zainab@wonderbly.com>
2021-10-08 14:54:15 +0100

    remove emoji

--- src/components/nav-content.jsx
+++ src/components/nav-content.jsx
@@ -9,7 +9,7 @@ export default function NavContent() {
 <p><b>On Android</b> click the ⋮ menu button then ‘install app’</p>
 <p>This app also works offline</p>
 
-<p>Feel free to contact me at <a className="email" href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a> 📧  with any corrections or suggestions</p>
+<p>Feel free to contact me at <a className="email" href="mailto:zainabismail024@gmail.com">zainabismail024@gmail.com</a> with any corrections or suggestions</p>
             
             <p><b>Sources</b></p>
             <p>Illustrations by Eman Atef</p>
