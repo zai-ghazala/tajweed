c91abc5e91548cb2a9ddd0d8a228ce5bfb8a48e9
zai-i <zainab@wonderbly.com>
2023-01-27 22:45:31 +0000

    Improvements PWA

--- vite.config.js
+++ vite.config.js
@@ -13,14 +13,12 @@ export default defineConfig({
         skipWaiting: true
       },
       includeAssets: [
-        "icons/favicon.ico",
-        "robots.txt",
-        "icons/apple-touch-icon.png",
+        "/icons/favicon.ico",
         "assets/fonts/*.ttf", 
         "assets/images/*.{png,jpeg}", 
         "assets/diagrams/*.png", 
         "assets/audio/*.mp3",
-        "pwaicons/*.png"],
+        "/icons/pwaicons/*.png"],
       manifest: {
         short_name: "Tajweed",
         name: "Tajweed",
@@ -28,23 +26,23 @@ export default defineConfig({
         theme_color: "#cc95c0",
         background_color: "#cc95c0",
         orientation: "portrait",
-        start_url: "./",
+        start_url: "/",
         scope: ".",
         display: "standalone",
         icons: [
           {
-            src: "icons/favicon.ico",
+            src: "/icons/favicon.ico",
             sizes: "48x48",
             type: "image/x-icon",
           },
           {
-            src: "icons/android-chrome-192x192.png",
+            src: "/icons/android-chrome-192x192.png",
             type: "image/png",
             sizes: "192x192",
             purpose: "maskable any",
           },
           {
-            src: "icons/android-chrome-512x512",
+            src: "/icons/android-chrome-512x512",
             type: "image/png",
             sizes: "512x512",
             purpose: "maskable any",
